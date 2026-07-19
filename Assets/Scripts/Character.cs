2a1d765c796340e5ac0268d8f0ef75d9217fb895
UnderworldArts <louis@tuatara.cc>
2026-07-20 11:28:47 +1200

    Better Character UI

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Character", menuName = "Create Character")]
 public class Character : ScriptableObject
 {
+    //Add Class sprite
+
+    //Class stats
     public int maxHealth;
     public int muscle;
     public int reflex;
